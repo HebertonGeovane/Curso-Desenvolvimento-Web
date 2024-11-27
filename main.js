dd0ebb90abdec3f3ed2be3d169ac754edd8c4aa3
Heberton Geovane <heberton.geovane@gmail.com>
2024-11-27 05:28:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let firstName = 'Heberton'
+let lastname =  'Geovane'
+let age = 39 
+
+console.log('Meu nome é ' + firstName + ' ' + lastname)
+console.log('Eu tenho' + ' '  +  age + ' ' + 'Anos')
+
+
+
+
+
+
+
+
+
